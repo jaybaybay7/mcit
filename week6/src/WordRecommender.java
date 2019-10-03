edb3803270e334dea33fdfbbd8ac51754a4b5051
jaymag <jaymag@172.20.10.2>
2019-10-03 08:26:11 -0700

    setting up spell checker

---
+++ week6/src/WordRecommender.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class WordRecommender {
+	private String filename;
+
+	public WordRecommender(String fileName) {
+		//
+	}
+	
+	 public double getSimilarityMetric(String word1, String word2) {
+		 return;
+	 }
+
+	 public ArrayList<String> getWordSuggestions(String word, int
+			 n, double commonPercent, int topN){
+		 //given an incorrect word, return a list of legal word 
+		 //suggestions as per an algorithm given below.
+	 }
+	 
+	 public ArrayList<String> getWordsWithCommonLetters(String word,
+			 ArrayList<String> listOfWords, int n){
+//		 /Given a word and a list of words from a dictionary, return the list of words in the dictionary that
+		 //have at least (>=) n letters in common.
+	 }
+	 
+	 public String prettyPrint(ArrayList<String> list) {
+		 
+	 }
+}
+
+
