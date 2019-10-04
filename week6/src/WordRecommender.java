1a65197c208436bccf9c7636fa1957feaf104ad2
jaymag <jaymag@fe80:0:0:0:7f56:b05b:a86a:d339%13>
2019-10-03 19:32:05 -0700

    working on first method to check similarity

--- week6/src/WordRecommender.java
+++ week6/src/WordRecommender.java
@@ -4,11 +4,61 @@ public class WordRecommender {
 	private String filename;
 
 	public WordRecommender(String fileName) {
-		//
+		this.filename = fileName;
 	}
 	
-	 public double getSimilarityMetric(String word1, String word2) {
-		 return;
+	
+	/*
+	 * given two words, this function computes two measures 
+	 * of similarity and returns the average.
+	 */
+	 
+	
+	//helper method to return which of the two words is longer
+	//used in the getSimilarityMetric method
+	public int returnLongerWord(String word1, String word2) {
+		if (word1.length() > word2.length()) {
+			return word1.length();
+		} else {
+			return word2.length();
+		}
+	}
+	
+	public double getSimilarityMetric(String word1, String word2) {
+		//Todo figure out how to handle words without the same length
+		 
+		 int leftSim = 0;
+		 int rightSim = 0;
+		 
+		 StringBuilder w1r = new StringBuilder();
+		 StringBuilder w2r = new StringBuilder();
+		 
+		 //w1r is now word1 backwards
+		 w1r.append(word1);
+		 w1r.reverse();
+		 
+		//w2r is now word2 backwards
+		 w2r.append(word2);
+		 w2r.reverse();
+		 
+		 
+		 //Left to right letter check
+		 for (int i = 0; i < returnLongerWord(word1, word2); i++) {
+			 if (word1.charAt(i) == word2.charAt(i)) {
+				 leftSim++;
+			 }
+		 }
+		 
+		 //righjt to left letter check
+		 for (int j = 0; j < returnLongerWord(word1, word2); j++) {
+			 if(w1r.charAt(j) == w2r.charAt(j)) {
+				 rightSim++;
+			 }
+		 }
+		 
+		 double simMetric = (leftSim + rightSim) / 2;
+		 
+		 return simMetric;
 	 }
 
 	 public ArrayList<String> getWordSuggestions(String word, int
@@ -27,6 +77,13 @@ public class WordRecommender {
 	 public String prettyPrint(ArrayList<String> list) {
 		 
 	 }
+	 
+	 public static void main(String[] args) {
+		 WordRecommender wr = new WordRecommender("wordtedster_good");
+		 
+		 System.out.println(wr.getSimilarityMetric("dea", "eaita"));
+	 }
 }
+
 
 
---
+++ week6/src/WordRunner.java
@@ -0,0 +1,9 @@
+
+public class WordRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
