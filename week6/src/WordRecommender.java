9a09632eac58273a341004620f595ea58f19c349
jaymag <jaymag@fe80:0:0:0:7f56:b05b:a86a:d339%13>
2019-10-03 20:00:39 -0700

    Finished getSimilarityMetric method

--- week6/src/WordRecommender.java
+++ week6/src/WordRecommender.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordRecommender {
 	private String filename;
@@ -6,25 +7,28 @@ public class WordRecommender {
 	public WordRecommender(String fileName) {
 		this.filename = fileName;
 	}
-	
-	
+
 	/*
-	 * given two words, this function computes two measures 
-	 * of similarity and returns the average.
+	 * given two words, this function computes two measures of similarity and
+	 * returns the average.
 	 */
-	 
-	
-	//helper method to return which of the two words is longer
-	//used in the getSimilarityMetric method
-	public int returnLongerWord(String word1, String word2) {
-		if (word1.length() > word2.length()) {
-			return word1.length();
+
+	// helper method to return which of the two words is shorter
+	// used in the getSimilarityMetric method
+	public String returnShorterWord(String firstWord, String secondWord) {
+		if (firstWord.length() < secondWord.length()) {
+			return firstWord;
 		} else {
-			return word2.length();
+			return secondWord;
 		}
 	}
+
+	/*
+	 * method for returning similarity. Uses returnShorterWord method
+	 */
 	
 	public double getSimilarityMetric(String word1, String word2) {
+		
 		//Todo figure out how to handle words without the same length
 		 
 		 int leftSim = 0;
@@ -43,47 +47,42 @@ public class WordRecommender {
 		 
 		 
 		 //Left to right letter check
-		 for (int i = 0; i < returnLongerWord(word1, word2); i++) {
+			for (int i = 0; i < returnShorterWord(word1, word2).length(); i++) {
 			 if (word1.charAt(i) == word2.charAt(i)) {
 				 leftSim++;
 			 }
 		 }
 		 
 		 //righjt to left letter check
-		 for (int j = 0; j < returnLongerWord(word1, word2); j++) {
+		 for (int j = 0; j < returnShorterWord(word1, word2).length(); j++) {
 			 if(w1r.charAt(j) == w2r.charAt(j)) {
 				 rightSim++;
 			 }
 		 }
 		 
-		 double simMetric = (leftSim + rightSim) / 2;
+		 double simMetric = (Double.valueOf(leftSim) + Double.valueOf(rightSim)) / 2;
 		 
 		 return simMetric;
 	 }
 
-	 public ArrayList<String> getWordSuggestions(String word, int
-			 n, double commonPercent, int topN){
-		 //given an incorrect word, return a list of legal word 
-		 //suggestions as per an algorithm given below.
-	 }
-	 
-	 public ArrayList<String> getWordsWithCommonLetters(String word,
-			 ArrayList<String> listOfWords, int n){
+	public ArrayList<String> getWordSuggestions(String word, int n, double commonPercent, int topN) {
+		// given an incorrect word, return a list of legal word
+		// suggestions as per an algorithm given below.
+	}
+
+	public ArrayList<String> getWordsWithCommonLetters(String word, ArrayList<String> listOfWords, int n) {
 //		 /Given a word and a list of words from a dictionary, return the list of words in the dictionary that
-		 //have at least (>=) n letters in common.
-		 //todo
-	 }
-	 
-	 public String prettyPrint(ArrayList<String> list) {
-		 
-	 }
-	 
-	 public static void main(String[] args) {
-		 WordRecommender wr = new WordRecommender("wordtedster_good");
-		 
-		 System.out.println(wr.getSimilarityMetric("dea", "eaita"));
-	 }
+		// have at least (>=) n letters in common.
+		// todo
+	}
+
+	public String prettyPrint(ArrayList<String> list) {
+
+	}
+
+	public static void main(String[] args) {
+		WordRecommender wr = new WordRecommender("wordtedster_good");
+
+		System.out.println(wr.getSimilarityMetric("h", "hello"));
+	}
 }
-
-
-
